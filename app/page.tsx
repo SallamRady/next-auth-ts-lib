bcb963b97fcc3a9fe826f1846ef4150294fd1e06
Sallam Rady <sallamrady@gmail.com>
2024-10-07 15:43:12 +0300

    show user name in case of authenticated from session data

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,18 @@
+"use client";
+
+import { useSession } from "next-auth/react";
+
 export default function Home() {
-  return <div>Home Page</div>;
+  const session = useSession();
+
+  if (session.status == "loading") return <h2>Loading...</h2>;
+  return (
+    <div>
+      <h1>
+        {session.status == "authenticated"
+          ? `Welcome ${session.data.user?.name} to Home Page`
+          : "Home Page"}
+      </h1>
+    </div>
+  );
 }
