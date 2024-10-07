b127130a4707e046cdf2874aad9c06244a83922f
Sallam Rady <sallamrady@gmail.com>
2024-10-07 15:30:37 +0300

    protect pages using useSession and getSession ways

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,3 +1,19 @@
-export default function BlobPage(){
-    return <>Blob Page</>
+"use client";
+
+import { getSession, signIn } from "next-auth/react";
+import { useEffect, useState } from "react";
+
+export default function BlobPage() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function ProtectPage() {
+      const session = await getSession();
+      if (!session) signIn();
+      else setLoading(false);
+    }
+    ProtectPage();
+  }, []);
+  if (loading) return <h2>Loading...</h2>;
+  return <>Blob Page</>;
 }
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,14 @@
-export default function DashboardPage(){
-    return <>Dashboard Page</>
+"use client";
+import { signIn, useSession } from "next-auth/react";
+
+export default function DashboardPage() {
+  const session = useSession();
+  console.log("sesssssssion data", session.data, session.status);
+  //protect page using useSession hook
+  if (session.status === "unauthenticated") {
+    signIn();
+  }
+
+  if (session.status == "loading") return <h3>Loading....</h3>;
+  return <>Dashboard Page</>;
 }
